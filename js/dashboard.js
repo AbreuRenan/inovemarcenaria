bfe9cf20ef4c3bc546fdc56d81a38fc790be0101
AbreuRenan <renan.de.abreu@gmail.com>
2023-06-22 19:43:55 -0300

    corrigido bug comportamento sidenave para dispositivos mobile

--- js/dashboard.js
+++ js/dashboard.js
@@ -95,4 +95,5 @@ function alertaCadastro(serverResponse) {
     }
 
 }
+
 console.log('dashboard loaded')
--- src/formCadProd.php
+++ src/formCadProd.php
@@ -1,14 +1,14 @@
 <form id="form_cad_produto" class="container p-2" enctype="multipart/form-data" action="../src/produtos_crud.php" method="POST">
     <div id="statusmsg" class="statusmsg alert" style="display: none"></div>
-    <div class="col row">
-        <div class="col-8 d-flex flex-column gap-2">
+    <div class="col row row-cols-1 row-cols-sm-2">
+        <div class="col col-sm-8 d-flex flex-column gap-2">
             <label class="form-label" for="nome_produto">Nome Produto</label>
             <input class="form-control" type="text" name="nome_produto" id="nome_produto"
                 placeholder="Digite o nome do produto para cadastro">
             <label class="form-label" for="preco_produto">Preço Inicial</label>
             <input class="form-control" type="money" name="preco_produto" id="preco_produto">
         </div>
-        <div class="col-4">
+        <div class="col col-sm-4 ">
             <div id="img_preview_container">
                 <img id="img_preview" src="" alt="" class="w-100" style="height: 120px; object-fit: cover;">
             </div>
@@ -25,6 +25,4 @@
         <input type="submit" class="btn btn-primary">
     </div>
     <input type="hidden" value="insert" name="operation">
-    <!-- <input type="hidden" name="id" id="id" value=""> -->
-
 </form>
